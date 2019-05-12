feb223a3b923233fc8769502f228522bc660af8f
vanbumi <jsp.dio@gmail.com>
2019-05-12 13:45:04 +0700

    koreksi kode app.js bagian2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <h1>Welcome to React Hook Course Mediocademy!</h1>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to React Hook Course Mediocademy!</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
