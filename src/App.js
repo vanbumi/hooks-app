c66475bc88e768f45e4ad5f7dffcd0519043c526
vanbumi <jsp.dio@gmail.com>
2019-06-06 11:37:17 +0700

    Kode bagian6 done

--- src/App.js
+++ src/App.js
@@ -1,47 +1,14 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class Button extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activated: false,
-    }
-    this.handleActiveChange = this.handleActiveChange.bind(this);
-  }
+function App() {
 
-  handleActiveChange() {
-    // this.setState({ 
-    //   activated: !this.state.activated 
-    // })
+  console.log(useState('nilai'));
 
-    this.setState((prevState) => {
-      return {
-        activated: !prevState.activated
-      }
-    })
-  }
-
-  render() {
-    const buttonText = this.state.activated ? this.props.activeText : this.props.inactiveText;
-
-    return (
-      <button onClick={this.handleActiveChange}>
-        {buttonText}
+  return (
+    <button>
+      The text
       </button>
-    )
-  }
-}
-
-// class base component
-class App extends Component {
-  render() {
-    return (
-      <Button
-        activeText="ON"
-        inactiveText="OFF"
-      />
-    )
-  }
+  )
 }
 
 export default App;
