9b31a2d26a5bc9c0aaf0e6e173e2474f003f517f
vanbumi <jsp.dio@gmail.com>
2019-06-06 16:55:39 +0700

    materi bagian 7 done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React, { useState } from 'react';
 
 function App() {
 
-  console.log(useState('nilai'));
+  const [activated, setActivated] = (useState(false));
+
+  // cek status dari activated dengan menggunakan button
+  const buttonText = activated ? 'Active' : 'Inactive';
+
+  function onClick() {
+    setActivated(!activated);
+  }
+
+  //const onClick = () => setActivated(!activated);
 
   return (
-    <button>
-      The text
+    <button onClick={onClick}>
+      {buttonText}
     </button>
   )
 }
