331479196235074e55a811b76755ee8091def448
vanbumi <jsp.dio@gmail.com>
2019-06-13 17:11:24 +0700

    materi bagian 9 done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React, { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
-  const handleClick = () => setCount((prevCount) => {
-    return prevCount + 1;
-  });
-
+  const handleIncrease = () => setCount(count + 1);
+  const handleDecrese = () => setCount(count - 1);
+  // const handleReset = () => setCount(0);
+  function handleReset() {
+    setCount(0);
+  }
 
   return (
     <div>
-      <button onClick={handleClick}>Increase Count</button>
+      <button onClick={handleIncrease}>Increase Count</button>
+      <button onClick={handleDecrese}>Decrease Count</button>
+      <button onClick={handleReset}>Reset Count</button>
       <h1>{count}</h1>
     </div>
   )
