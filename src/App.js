5c0784589776f0b98507aacbe5a99ad970b2f6a3
vanbumi <jsp.dio@gmail.com>
2019-11-29 05:34:36 +0700

    materi bagian 13.2 done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React, { useState, useEffect } from 'react';
 
+function Counter() {
+    const [count, setCount] = useState(0);
+    const handleIncrease = () => setCount(count + 1);
+    const handleDecrease = () => setCount(count - 1);
+  
+    useEffect(() => {
+      console.log(`Ini adalah useEffect function, current count adalah ${count}`)
+  
+      return () => {
+        console.log(`Saya bertugas me-remove semua setup di atas! Last count adalah ${count}`)
+      }
+    })
+  
+    return (
+      <div>
+        <button onClick={handleIncrease}>Increase</button>
+        <button onClick={handleDecrease}>Decrease</button>
+  
+        <h1>{count}</h1>
+      </div>
+    )
+}
+
 function App() {
-  const [count, setCount] = useState(0);
-  const handleIncrease = () => setCount(count + 1);
-  const handleDecrease = () => setCount(count - 1);
+    const [visible, setVisible] = useState(false);
 
-  // tambah useEffect
-  useEffect(() => {
-    console.log(`Ini adalah useEffect function, current count adalah ${count}`)
+    return (
+        <div>
+            <button onClick={() => setVisible(!visible)}>
+                Show / HIde Counter
+            </button>
 
-    return () => {
-      console.log(`Saya bertugas me-remove semua setup di atas! Last count adalah ${count}`)
-    }
-  })
-
-  return (
-    <div>
-      <button onClick={handleIncrease}>Increase</button>
-      <button onClick={handleDecrease}>Decrease</button>
-
-      <h1>{count}</h1>
-    </div>
-  )
+            { visible && <Counter /> }
+        </div>
+    )
 }
 
 export default App;
